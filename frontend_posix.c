f2f8e7dadabc54b946d600449b74992d71341640
Richard Mongler <chaiyac@pdx.edu>
2024-06-06 23:23:12 -0700

    Moved out readchar, writechar, terminal setup/teardown fns into separate platform-specific files

---
+++ frontend_posix.c
@@ -0,0 +1,72 @@
+#include "emulation-core/SoC.h"
+
+#include <stdio.h>
+#include <time.h>
+#include <sys/select.h>
+#include <termios.h>
+#include <unistd.h>
+
+int readchar(int *ctlCSeen) {
+	// Do a non-blocking getchar, synthesizing control-c using
+	// a custom SIGINT handler, and returning CHAR_NONE otherwise
+
+	struct timeval tv;
+	fd_set set;
+	char c;
+	int i, ret = CHAR_NONE;
+
+	if (ctlCSeen) {
+		ctlCSeen = 0;
+		return 0x03;
+	}
+
+	tv.tv_sec = 0;
+	tv.tv_usec = 0;
+
+	FD_ZERO(&set);
+	FD_SET(0, &set);
+
+	i = select(1, &set, NULL, NULL, &tv);
+	if (i == 1 && 1 == read(0, &c, 1)) {
+
+		ret = c;
+	}
+
+	return ret;
+}
+
+void writechar(int chr) {
+	if (!(chr & 0xFF00)) {
+
+		printf("%c", chr);
+	}
+	else {
+		printf("<<~~ EC_0x%x ~~>>", chr);
+	}
+	fflush(stdout);
+}
+
+static struct termios cfg, old;
+
+void setupTerminal() {
+	int ret;
+
+	ret = tcgetattr(0, &old);
+	cfg = old;
+	if (ret) perror("cannot get term attrs");
+
+	#ifndef __APPLE__
+		cfg.c_iflag &= ~(INLCR | INPCK | ISTRIP | IUCLC | IXANY | IXOFF | IXON);
+		cfg.c_oflag &= ~(OPOST | OLCUC | ONLCR | OCRNL | ONOCR | ONLRET);
+		cfg.c_lflag &= ~(ECHO | ECHOE | ECHONL | ICANON | IEXTEN | XCASE);
+	#else
+		cfmakeraw(&cfg);
+	#endif
+
+	ret = tcsetattr(0, TCSANOW, &cfg);
+	if (ret) perror("cannot set term attrs");
+}
+
+void teardownTerminal() {
+	tcsetattr(0, TCSANOW, &old);
+}
--- main_pc.c
+++ main_pc.c
@@ -6,16 +6,19 @@
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#ifndef _MSC_VER
+	#include <unistd.h>
 	// Needed for ftruncate
+#else
+	#include <io.h>
+	#define ftruncate _chsize_s
+#endif
 #include <sys/types.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <sys/select.h>
 #include <signal.h>
-#include <termios.h>
-=======
+
 
 // DECLARATIONS for frontend functions to be defined in a frontend_*.c file.
 // For standard POSIX, see frontend_POSIX.h.
@@ -24,7 +27,6 @@ void writechar(int chr);
 void setupTerminal();
 void teardownTerminal();
 //
->>>>>>> Stashed changes
 
 unsigned char* readFile(const char* name, UInt32* lenP){
 
@@ -74,55 +76,21 @@ unsigned char* readFile(const char* name, UInt32* lenP){
 	return r;
 }
 
-
-
 static int ctlCSeen = 0;
-
-static int readchar(void){
-	// Do a non-blocking getchar, synthesizing control-c using
-	// a custom SIGINT handler, and returning CHAR_NONE otherwise
-	
-	struct timeval tv;
-	fd_set set;
-	char c;
-	int i, ret = CHAR_NONE;
-	
-	if(ctlCSeen){
-		ctlCSeen = 0;
-		return 0x03;
-	}
-	
-	tv.tv_sec = 0;
-	tv.tv_usec = 0;
-	
-	FD_ZERO(&set);
-	FD_SET(0, &set);
-	
-	i = select(1, &set, NULL, NULL, &tv);
-	if(i == 1 && 1 == read(0, &c, 1)){
-		
-		ret = c;
-	}
-
-	return ret;
-}
-
-static void writechar(int chr){
-
-	if(!(chr & 0xFF00)){
-		
-		printf("%c", chr);
-	}
-	else{
-		printf("<<~~ EC_0x%x ~~>>", chr);
-	}
-	fflush(stdout);	
-}
 
 void ctl_cHandler(_UNUSED_ int v){	//handle SIGTERM      
 	
 //	exit(-1);
 	ctlCSeen = 1;
+}
+
+int readchar_wrapper() {
+	// Why do we need a 'wrapper' for readchar (originally defined in
+	// frontend_posix.h, etc.?)
+	// We need to pass to readchar(int*) a piece of state - the flag
+	// set by the control-c handler. uARM's SoC code doesn't do this
+	// plumbing for us.
+	return readchar(&ctlCSeen);
 }
 
 int rootOps(void* userData, UInt32 sector, void* buf, UInt8 op){
@@ -237,7 +205,6 @@ Boolean coRamAccess(_UNUSED_ void* ram, UInt32 addr, UInt8 size, Boolean write, 
 
 int main(int argc, char** argv){
 	
-	struct termios cfg, old;
 	FILE* root = NULL;
 	int gdbPort = 0;
 	
@@ -249,25 +216,7 @@ int main(int argc, char** argv){
 	
 	
 	//setup the terminal
-	{
-		int ret;
-		
-		ret = tcgetattr(0, &old);
-		cfg = old;
-		if(ret) perror("cannot get term attrs");
-		
-		#ifndef __APPLE__
-		
-			cfg.c_iflag &=~ (INLCR | INPCK | ISTRIP | IUCLC | IXANY | IXOFF | IXON);
-			cfg.c_oflag &=~ (OPOST | OLCUC | ONLCR | OCRNL | ONOCR | ONLRET);
-			cfg.c_lflag &=~ (ECHO | ECHOE | ECHONL | ICANON | IEXTEN | XCASE);
-		#else
-			cfmakeraw(&cfg);
-		#endif
-		
-		ret = tcsetattr(0, TCSANOW, &cfg);
-		if(ret) perror("cannot set term attrs");
-	}
+	setupTerminal();
 	
 	// custom SIGINT (ctrl-c) handler, that sets a flag to
 	// pass along an interrupt char to the emulated OS
@@ -327,7 +276,7 @@ int main(int argc, char** argv){
 	}
 	
 	// Set up SoC, and then run the main loop
-	socInit(&soc, socRamModeCallout, coRamAccess, readchar, writechar, rootOps, root);
+	socInit(&soc, socRamModeCallout, coRamAccess, readchar_wrapper, writechar, rootOps, root);
 	
 	socRunState runState;
 	socRunStateInit(&runState);
@@ -361,7 +310,8 @@ int main(int argc, char** argv){
 	
 	//cleanup opened files
 	fclose(root);
-	tcsetattr(0, TCSANOW, &old);
+
+	teardownTerminal();
 	
 	return 0;
 }
