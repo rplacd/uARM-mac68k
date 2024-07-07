05c75b56d0d6a5c8a973d0f1d37508e3ba49ccc7
Richard Mongler <chaiyac@pdx.edu>
2024-07-07 06:35:26 -0700

    Create new posix-interleaved-async-term frontend
    The idea is that we interleave servicing the terminal frontend, and byte reads + writes to ramfile (the biggest time hog)
    Current frontend in folder is a stub frontend that simply services frontend interaction every loop (i.e. synchronously again.)

---
+++ other-platforms-and-frontends/posix-interleaved-async-term/frontend_interleaved_asyncterm.c
@@ -0,0 +1,222 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <signal.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <termios.h>
+
+#include "../../emulation-core/SoC.h"
+#include "../../deps/FIFO/FIFO.h"
+
+static struct termios cfg, old;
+
+int ctlCSeen;
+
+// Input/output buffer state.
+static fifo_t inputBuf;
+static fifo_t outputBuf;
+#define IO_BUF_SIZ 20
+
+// Timer interrupt state.
+static sigset_t timer_callback_s;
+#define FRONTEND_PERIOD_USEC 100 // e.g. 1 ms.
+
+// What signal type/interval timer can we use
+// to service IO? On DJGPP (i.e. single-process DOS),
+// we use a "real time" alarm, in contrast to...
+#ifdef __DJGPP__
+	#define OUR_TIMERSIG SIGALRM
+	#define OUR_WHICHTIMER ITIMER_REAL
+#else
+// ...on multiprocess POSIX platforms, where we use
+// "process time" alarm.
+	#define OUR_TIMERSIG SIGVTALRM
+	#define OUR_WHICHTIMER ITIMER_VIRTUAL
+#endif
+
+// Utility definitions that will let us expand OUR_SIGNALTYPE
+// into a string constant.
+#define STR(str) #str
+#define STRING(str) STR(str)
+
+
+// TODO: on DJGPP, 
+// why do we finish setupTerminal,
+// but never get to service_frontend?
+// Are we setting up the timer callback
+// properly?
+
+void service_frontend() {
+	fflush(stdout);
+	
+	// Do async, non-blocking (to the SoC) terminal IO.
+	
+	// ...print output and flush;
+	int outputChar;
+	while(fifo_get(outputBuf, &outputChar)) {
+		if(outputChar == CHAR_NONE) {
+			continue;
+		} else if(outputChar == CHAR_CTL_C) {
+			// this case should (probably) not be
+			// happening; my guess is that CHAR_CTL_C
+			// is only used by SoC input routines,
+			// but I haven't checked this.
+			fprintf(stderr, "output: CHAR_CTL_C found"
+				"in output buffer; don't know what to do\n");
+			exit(0);
+		} else if(!(outputChar & 0xFF00)) {
+			printf("%c", outputChar);
+			fflush(stdout);
+			continue;
+		} else {
+			printf("<<~~ EC_0x%x ~~>>", outputChar);
+			fflush(stdout);
+			continue;
+		}
+	}
+	
+	// ...consume input IF the input FIFO is empty AND
+	// 
+	// what happens when input "piles up" isn't
+	// our responsibility.
+	struct timeval tv;
+	tv.tv_sec = 0;
+	tv.tv_usec = 0;
+	fd_set set;
+	FD_ZERO(&set);
+	FD_SET(0, &set);
+	
+	int toAdd = 0xDEAD;
+		// Sentintel value here;
+	int i;
+	char c;
+		
+	while(!fifo_is_full(inputBuf)) {
+	
+		// TODO: note that control-c input is not
+		// guaranteed to be handled in order of actual
+		// input. we'll see whether this bites us in the
+		// ass.
+		if(ctlCSeen) {
+			ctlCSeen = 0;
+			toAdd = CHAR_CTL_C;
+			goto add_to_buf;
+		}
+
+		i = select(1, &set, NULL, NULL, &tv);
+		if (i == 1 && 1 == read(0, &c, 1)) {
+			toAdd = c;
+			goto add_to_buf;
+		} else {
+			// NB – this is the only termination condition
+			// for this loop.
+			goto done;
+		}
+
+		add_to_buf:
+		#ifndef NDEBUG
+		if(toAdd == 0xDEAD) {
+			fprintf(stderr, "programmer error: attempting to"
+				"add item to input buffer, but we didn't read"
+				"anything before getting here\n");
+			exit(-1);
+		}
+		#endif
+		if(!fifo_add(inputBuf, &toAdd)) {
+			fprintf(stderr, "programmer error: while adding"
+				"input to input buffer, input buffer not empty\n");
+			exit(-1);
+		}
+	}
+	
+	done:
+	return;
+}
+
+int readchar(int* oobCtlCSeen) {
+	// note that control-c input is handled
+	// separately of our async IO by design
+	// in main_pc.c ("out of band"), 
+	// and so we actually handle putting it
+	// into the input buffer here.
+
+	// NB – we don't handle returning a CHAR_CTL_C
+	// directly here: this is because it's possible
+	// in this case that an input sequence
+	// (char) (control-C) might end up being read
+	// in the order (control-C) (char) by the SoC.
+	if(*oobCtlCSeen) {
+		*oobCtlCSeen = 0;
+		ctlCSeen = 1;
+		return CHAR_NONE;
+	} else if(!fifo_is_empty(inputBuf))  {
+		int ret;
+		bool valid_call = fifo_get(inputBuf, &ret);
+		if(!valid_call) {
+			perror("Program error: for input buffer, !fifo_is_empty but no item to get from FIFO");
+			exit(-1);
+		}
+		return ret;
+	} else {
+		return CHAR_NONE;
+	}
+}
+
+void writechar(int chr) {
+	if(chr == CHAR_NONE) {
+		return;
+	}	
+
+	// Block if no space to batch write; wait for async IO
+	// to empty output buffer
+	// (hope period is short enough to maintain interactivity).
+	while(fifo_is_full(outputBuf)) {
+		continue;
+	}
+	
+	bool valid_call = fifo_add(outputBuf, &chr);
+	
+	if(!valid_call)  {
+		perror("Program error: for output buffer,"
+			"!fifo_is_empty but no item to get from FIFO");
+		exit(-1);
+	}
+}
+
+void setupTerminal() {
+	// Setup terminal raw mode;
+	int ret;
+
+	ret = tcgetattr(0, &old);
+	cfg = old;
+	if (ret) perror("cannot get term attrs");
+	
+	#if !(defined(__APPLE__)) && !(defined(__DJGPP__))
+		cfg.c_iflag &= ~(INLCR | INPCK | ISTRIP | IUCLC | IXANY | IXOFF | IXON);
+		cfg.c_oflag &= ~(OPOST | OLCUC | ONLCR | OCRNL | ONOCR | ONLRET);
+		cfg.c_lflag &= ~(ECHO | ECHOE | ECHONL | ICANON | IEXTEN | XCASE);
+	#else
+		cfmakeraw(&cfg);
+	#endif
+	
+	ret = tcsetattr(0, TCSANOW, &cfg);
+	if (ret) perror("cannot set term attrs");
+	
+	// Init globals;
+	ctlCSeen = 0;
+	
+	// Setup output buffer;
+	inputBuf = fifo_create(IO_BUF_SIZ, sizeof(int));
+	outputBuf = fifo_create(IO_BUF_SIZ, sizeof(int));
+}
+
+void teardownTerminal() {
+	tcsetattr(0, TCSANOW, &old);
+}
+
+void cycleProlog() {
+}
+
+void cycleEpilog() {
+}
---
+++ other-platforms-and-frontends/posix-interleaved-async-term/main_pc.c
@@ -0,0 +1,372 @@
+#include "../../utilities/compiler_hacks.h"
+
+#include "../../emulation-core/SoC.h"
+#include "../../emulation-core/callout_RAM.h"
+
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#ifndef _MSC_VER
+	#include <unistd.h>
+	// Needed for ftruncate
+#else
+	#include <io.h>
+	#define ftruncate _chsize_s
+#endif
+#include <sys/types.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <signal.h>
+
+
+// DECLARATIONS for frontend functions to be defined in a frontend_*.c file.
+// For standard POSIX, see frontend_POSIX.h.
+int readchar(int* ctlCSeen);
+void writechar(int chr);
+void setupTerminal();
+void teardownTerminal();
+// A special routine, defined in frontend_asyncterm.c, to service
+// the UI (i.e. read/print chars) and maintain interactivity.
+void service_frontend();
+
+unsigned char* readFile(const char* name, UInt32* lenP){
+
+	long len = 0;
+	unsigned char *r = NULL;
+	int i;
+	FILE* f;
+	
+	f = fopen(name, "r");
+	if(!f){
+		perror("cannot open file");
+		return NULL;
+	}
+	
+	i = fseek(f, 0, SEEK_END);
+	if(i){
+		return NULL;
+		perror("cannot seek to end");
+	}
+	
+	len = ftell(f);
+	if(len < 0){
+		perror("cannot get position");
+		return NULL;
+	}
+	
+	i = fseek(f, 0, SEEK_SET);
+	if(i){
+		return NULL;
+		perror("cannot seek to start");
+	}
+	
+	
+	r = (unsigned char*)malloc(len);
+	if(!r){
+		perror("cannot alloc memory");
+		return NULL;
+	}
+	
+	if(len != (long)fread(r, 1, len, f)){
+		perror("canot read file");
+		free(r);
+		return NULL;
+	}
+	
+	*lenP = len;
+	return r;
+}
+
+static int ctlCSeen = 0;
+
+void ctl_cHandler(_UNUSED_ int v){	//handle SIGTERM      
+	
+//	exit(-1);
+	ctlCSeen = 1;
+}
+
+int readchar_wrapper() {
+	// Why do we need a 'wrapper' for readchar (originally defined in
+	// frontend_posix.h, etc.?)
+	// We need to pass to readchar(int*) a piece of state - the flag
+	// set by the control-c handler. uARM's SoC code doesn't do this
+	// plumbing for us.
+	return readchar(&ctlCSeen);
+}
+
+int rootOps(void* userData, UInt32 sector, void* buf, UInt8 op){
+	// Perform memory load/store operations via reading/writing
+	// to a ramfile. Because disk IO here is the biggest hog
+	// on performance, and because every time we call
+	// this function we read/write bytes in multiples
+	// of the constant BLK_DEV_BLK_SZ, we can interleave calls
+	// to service_frontend() with individual byte read/writes.
+	// Hopefully, the time it takes to read/write an individual byte
+	// is small enough (as opposed to an entire block) that running
+	// service_frontend() between bytes is enough to maintain
+	// interactivity.
+	
+	// NB: rootOps is of type ArmMemAccessF.
+	
+	FILE* root = (FILE*)userData;
+	int i;
+	
+	switch(op){
+		case BLK_OP_SIZE:
+			
+			if(sector == 0){	//num blocks
+				
+				if(root){
+					
+					i = fseek(root, 0, SEEK_END);
+					if(i) return false;
+					
+					 *(unsigned long*)buf = ftell(root) / BLK_DEV_BLK_SZ;
+				}
+				else{
+					
+					*(unsigned long*)buf = 0;
+				}
+			}
+			else if(sector == 1){	//block size
+				
+				*(unsigned long*)buf = BLK_DEV_BLK_SZ;
+			}
+			else return 0;
+			return 1;
+		
+		case BLK_OP_READ:
+			i = fseek(root, sector * BLK_DEV_BLK_SZ, SEEK_SET);
+			if(i) return false;
+			// Conceptually, we do:
+			// 		return fread(buf, 1, BLK_DEV_BLK_SZ, root) == BLK_DEV_BLK_SZ;
+			// but we instead turn this into 'BLK_DEV_BLK_SZ' iterations
+			// interleaved with service_frontend().
+			return fread(buf, 1, BLK_DEV_BLK_SZ, root) == BLK_DEV_BLK_SZ;
+		
+		case BLK_OP_WRITE:
+			
+			i = fseek(root, sector * BLK_DEV_BLK_SZ, SEEK_SET);
+			if(i) return false;
+			// Conceptually, we do:
+			// 		return fwrite(buf, 1, BLK_DEV_BLK_SZ, root) == BLK_DEV_BLK_SZ.
+			return fwrite(buf, 1, BLK_DEV_BLK_SZ, root) == BLK_DEV_BLK_SZ;
+	}
+	return 0;	
+}
+
+SoC soc;
+const char* ramFilePath = "16 MB of RAM";
+FILE *ramFile; // 16MB, per RAM_SIZE in SoC.c
+
+
+Boolean coRamAccess(_UNUSED_ void* ram, UInt32 addr, UInt8 size, Boolean write, void* bufP) {
+	// NB: coRamAccess is of type mem.h::ArmRamAccessF.
+	
+	// Below, we ran socInit(&soc, socRamModeCallout, coRamAccess, ...
+	// As a result, this fucntion (coRamAccess) is a callback that handles memory operations.
+
+	UInt8* b = (UInt8*)bufP;
+	
+	// The physical memory map seen by the SoC maps RAM to the region RAM_BASE, RAM_BASE + RAM_SIZE.
+	// Therefore, to map to RAM device adresses, subtract RAM_BASE.
+	addr = addr - RAM_BASE;
+	
+	#ifndef NDEBUG
+	// Optionally, double-check to see whether addr <= RAM_SIZE; this catches cases where
+	// 'addr' is a logical address instead of a physical one.
+	if(addr >= RAM_SIZE) {
+		printf("Error accessing ramfile: requested address larger than physical limits. Are you using logical addresses?\n");
+		printf("%x >= %lu\n", addr, RAM_SIZE);
+		exit(-1);
+	}
+	#endif
+	
+	if(fseek(ramFile, addr, SEEK_SET) != 0) {
+		perror("Error accessing location in ramfile");
+		exit(-1);
+	}
+
+	if(write) {
+		// pseudocode: ramWrite(addr, b, size);
+		fwrite(b, 1, size, ramFile);
+		
+		// the C standard doesn't specify how 'fwrite' will
+		// signal an error condition with its return value,
+		// so we manually check for error conditions:
+		if (feof(ramFile)) {
+			printf("Error writing to ramfile: end of file");
+			exit(-1);
+		} else if (ferror(ramFile)) {
+			perror("Error writing to ramfile");
+			exit(-1);
+		}
+	} else {
+		// pseudocode: ramRead(addr, b, size);
+		if(fread(b, 1, size, ramFile) != size) {
+			if (feof(ramFile)) {
+				printf("Error writing to ramfile: end of file");
+				exit(-1);
+			} else if (ferror(ramFile)) {
+				perror("Error writing to ramfile");
+				exit(-1);
+			} else {
+				printf("Error writing to ramfile: some other error");
+				exit(-1);
+			}
+		}
+	}
+
+	return true;
+}
+
+int main(int argc, char** argv){
+	
+	FILE* root = NULL;
+	int gdbPort = 0;
+	
+	if(argc <= 1 || argc >= 4){
+		PRINT_HELP_AND_QUIT:
+		fprintf(stderr,"usage: %s path_to_disk [with-telemetry] [gdbPort]]\n", argv[0]);
+		return -1;	
+	}
+	
+	
+	//setup the terminal
+	setupTerminal();
+	
+	// custom SIGINT (ctrl-c) handler, that sets a flag to
+	// pass along an interrupt char to the emulated OS
+
+	signal(SIGINT, &ctl_cHandler);
+	
+	
+	// handle argv[2] – boot image
+	root = fopen(argv[1], "r+b");
+	if(!root){
+		fprintf(stderr,"Failed to open root device\n");
+		exit(-1);
+	}
+	
+	// handle argv[3] - with-statusfile
+	Boolean withStatusFile = 0;
+	
+		// If with-statusfile is given, this is the state
+		// we will put into the statusfile
+	FILE *statusfile = NULL;
+	unsigned long totalCycles = 0;
+	time_t lastCycleAt = clock();
+	
+	if(argc >= 3) {
+		// The only item that can appear as a second param is "with-telemetry"
+		// but double check anyway
+		if(0 == strncmp("with-telemetry", argv[2], 15)) {
+			withStatusFile = 1;
+			
+			statusfile = fopen("Telemetry File", "w");
+			
+			if(NULL == statusfile) {
+				perror("Can't create or open statusfile");
+				exit(-1);
+			}
+	
+		} else goto PRINT_HELP_AND_QUIT;
+	}
+	
+	
+	// handle argv[4] - gdb port
+	
+	if(argc >= 4) gdbPort = atoi(argv[3]);
+	
+	
+	//setup the RAM disk
+	{
+		ramFile = fopen(ramFilePath, "w+b");
+		if (ramFile == NULL) {
+			perror("Creating/opening ramfile failed");
+			exit(-1);
+		}
+		if(ftruncate(fileno(ramFile), RAM_SIZE) != 0) {
+			perror("Expanding ramfile to 16MB (SoC.h/RAM_SIZE) failed");
+			exit(-1);
+		}
+	}
+	
+	// Set up SoC, and then run the main loop
+	socInit(&soc, socRamModeCallout, coRamAccess, readchar_wrapper, writechar, rootOps, root);
+	
+	socRunState runState;
+	socRunStateInit(&runState);
+	
+	Boolean going;
+	
+	// Main loop. Might choose to receive UI events here, for example.
+	do {
+		service_frontend();
+			// Service UI here, as well as while (interleaved with)
+			// ramfile read/writes...
+		
+		going = socCycle(&soc, gdbPort, &runState);
+		
+		if(withStatusFile) {
+		// Overwrite statusfile (that is, refresh status displayed.)
+			totalCycles++;
+			time_t now = clock();
+			time_t cycleTime = difftime(now, lastCycleAt);
+			lastCycleAt = now;
+			
+			rewind(statusfile);
+			
+			// write new status
+			fprintf(statusfile, "at time: %lu\ntotal cycles (possible overflow): %lu\ntime this cycle took: %lu\n",
+				now, totalCycles, cycleTime);
+			fflush(statusfile);
+		}
+	} while(going);
+	
+	//teardown the RAM disk
+	{
+		fclose(ramFile);
+	}
+	
+	//cleanup opened files
+	fclose(root);
+
+	teardownTerminal();
+	
+	return 0;
+}
+
+
+//////// runtime things
+
+void* emu_alloc(UInt32 size){
+	
+	return calloc(size,1);	
+}
+
+void emu_free(void* ptr){
+	
+	free(ptr);
+}
+
+UInt32 rtcCurTime(void){
+	/* the original implementation of uARM by D.G. used *nix 
+	   sys/time.h and returned a Unix timestamp: 
+			struct timeval tv;
+			gettimeofday(&tv, NULL);
+			return tv.tv_sec;	
+			
+		the following re-implmentation using C standard libraries
+		will (probably) do the exact same thing on *nixes with
+		sys/time.h, but its behavior on non-*nix platforms is unknown.
+	*/
+	return time(NULL);
+}
+
+void err_str(const char* str){
+	
+	fprintf(stderr, "%s", str);	
+}
+
